c5f3ed2c2fad8104bf99618149fe0d49786b0836
Wes Oldenbeuving <narnach@gmail.com>
2010-09-29 12:04:17 +0200

    Style consistency fix

--- lib/comcetera.rb
+++ lib/comcetera.rb
@@ -2,15 +2,15 @@ require 'open-uri'
 require 'timeout'
 
 class Comcetera
-  ErrorCodes={
-    "1"=>"Comcetra Unknown subscriber",      
-    "29"=>"Comcetra Absent subscriber",
-    "21"=>"Comcetra Facility not supported",
-    "11"=>"Comcetra Teleserice not provisioned",
-    "13"=>"Comcetra Call barred",
-    "36"=>"Comcetra System Failure"     
+  ERROR_CODES={
+    "1"=>"Unknown subscriber",
+    "29"=>"Absent subscriber",
+    "21"=>"Facility not supported",
+    "11"=>"Teleserice not provisioned",
+    "13"=>"Call barred",
+    "36"=>"System Failure"
   }
-    
+
   attr_accessor :operator_code, :msisdn
   attr_accessor :error_code, :error_message, :debug
 
@@ -30,7 +30,7 @@ class Comcetera
     def retries
       @retries ||= 2
     end
-    
+
     def password
       @password || raise("No password set for Comcetera")
     end
@@ -48,10 +48,10 @@ class Comcetera
           Timeout::timeout(self.timeout) do
             body=open("http://api.comcetera.com/npl?user=#{self.username}&pass=#{self.password}&msisdn=#{msisdn}").read
             msisdn, operator_code = body.split("\n")[1].split(" ") # 2nd line, last word is the operator hexcode
-            unless operator_code.to_s =~ /ERR(\d+)/              
+            unless operator_code.to_s =~ /ERR(\d+)/
               return new(:operator_code => operator_code, :msisdn => msisdn)
             else
-              return new(:operator_code => nil, :msisdn => msisdn, :error_code=>operator_code, :error_message=>ErrorCodes[$1]||"Unknown Error", :debug=>body)
+              return new(:operator_code => nil, :msisdn => msisdn, :error_code=>operator_code, :error_message=>ERROR_CODES[$1]||"Unknown Error", :debug=>body)
             end
           end
         rescue Timeout::Error, SystemCallError => e
